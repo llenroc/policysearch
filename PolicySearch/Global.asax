70f8e91be39a9f2f77d85429fc42482b348bc626
Andre Odendaal <aodendaal@gmail.com>
2017-01-19 14:20:36 +0200

    Initial commit

---
+++ PolicySearch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PolicySearch.WebApiApplication" Language="C#" %>
---
+++ PolicySearch/app/components/home/home.controller.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    angular
+        .module("app")
+        .component("homeController", homeController);
+
+    function homeController() {
+        let vm = this;
+
+        vm.title = "Home";
+    }
+})();
